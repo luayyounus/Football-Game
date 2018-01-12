d48fd86973a915e25d50aaef110b5080383d78a9
Luay <luayghazwan@yahoo.com>
2018-01-11 22:26:11 -0800

    Team model created

---
+++ FootballDeployment/FootballDeployment/Models/Team.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FootballDeployment.Models
+{
+    public class Team
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Country { get; set; }
+        public string Field { get; set; }
+        public Player[] Players { get; set; }
+    }
+}
