e2031e4e2b2539f1f1780ee85856b6ab29ffb097
Luay <luayghazwan@yahoo.com>
2018-01-11 22:51:28 -0800

    Post player action created with dbcontext ependency injection

--- FootballDeployment/FootballDeployment/Controllers/HomeController.cs
+++ FootballDeployment/FootballDeployment/Controllers/HomeController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FootballDeployment.Data;
+using FootballDeployment.Models;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -10,10 +12,29 @@ namespace FootballDeployment.Controllers
 {
     public class HomeController : Controller
     {
+        private FootballDbContext _context;
+
+        public HomeController(FootballDbContext context)
+        {
+            _context = context;
+        }
+
         // GET: /<controller>/
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(Player player)
+        {
+            //Add player to database
+            _context.Players.Add(player);
+            //Saving the player
+            _context.SaveChanges();
+
+            //Redirect to view team info
+            return RedirectToAction("TeamInfo", "Team");
         }
     }
 }
