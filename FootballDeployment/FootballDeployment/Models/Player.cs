a9ef58dffbafa3f1a55bb7da4390cb0927752ae0
Luay <luayghazwan@yahoo.com>
2018-01-11 21:39:43 -0800

    Startup configuration setup and Player Model

---
+++ FootballDeployment/FootballDeployment/Models/Player.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FootballDeployment.Models
+{
+    public class Player
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public byte Age { get; set; }
+        public byte Number { get; set; }
+        public string Club { get; set; }
+    }
+}
--- FootballDeployment/FootballDeployment/Startup.cs
+++ FootballDeployment/FootballDeployment/Startup.cs
@@ -5,16 +5,25 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace FootballDeployment
 {
     public class Startup
     {
+        // Dependency Injection for the configuration
+        private IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configruation)
+        {
+            Configuration = configruation;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -24,6 +33,12 @@ namespace FootballDeployment
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            // enabling statric files
+            app.UseStaticFiles();
+
+            // Setting manual routing
+            app.UseMvc(route => route.MapRoute(name: "default", template: "{controller=Home}/{action=Index}/{id?}"));
 
             app.Run(async (context) =>
             {
