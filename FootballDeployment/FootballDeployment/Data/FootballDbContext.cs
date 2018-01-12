d56e2785bffceef3192d02c8213fc59a8bf04e0e
Luay <luayghazwan@yahoo.com>
2018-01-11 22:30:34 -0800

    Database context created with Dbset for Player and Team

---
+++ FootballDeployment/FootballDeployment/Data/FootballDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FootballDeployment.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FootballDeployment.Data
+{
+    public class FootballDbContext : DbContext
+    {
+        public FootballDbContext(DbContextOptions<FootballDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Team> Teams { get; set; }
+        public DbSet<Player> Players { get; set; }
+    }
+}
