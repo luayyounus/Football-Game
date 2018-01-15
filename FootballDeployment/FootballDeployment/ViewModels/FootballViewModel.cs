6c5f2eb2aac1e207cbed93e236ed245660ee2f86
Luay <luayghazwan@yahoo.com>
2018-01-15 01:01:59 -0800

    Football view model creation to hold team and players

---
+++ FootballDeployment/FootballDeployment/ViewModels/FootballViewModel.cs
@@ -0,0 +1,14 @@
+﻿using FootballDeployment.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FootballDeployment.ViewModels
+{
+    public class FootballViewModel
+    {
+        public List<Player> Players { get; set; }
+        public Team Team { get; set; }
+    }
+}
