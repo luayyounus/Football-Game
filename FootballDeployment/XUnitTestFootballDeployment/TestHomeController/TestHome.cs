61770cd20a61653a6c21e3e34bb425d31a21510b
Luay <luayghazwan@yahoo.com>
2018-01-15 01:26:50 -0800

    Tests built for Home Controller

---
+++ FootballDeployment/XUnitTestFootballDeployment/TestHomeController/TestHome.cs
@@ -0,0 +1,51 @@
+using FootballDeployment.Controllers;
+using FootballDeployment.Data;
+using FootballDeployment.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Xunit;
+
+namespace XUnitTestFootballDeployment
+{
+    public class TestHome
+    {
+        FootballDbContext _context;
+        public TestHome()
+        {
+            DbContextOptionsBuilder<FootballDbContext> builder = new DbContextOptionsBuilder<FootballDbContext>();
+            builder.UseInMemoryDatabase();
+            DbContextOptions<FootballDbContext> options = builder.Options;
+            _context = new FootballDbContext(options);
+        }
+
+        [Fact]
+        public void Home_Index_Returns_View()
+        {
+            // Arrange
+            HomeController homeController = new HomeController(_context);
+
+            // Act
+            var result = homeController.Index();
+
+            // Assert
+            Assert.NotNull(result);
+        }
+
+        [Fact]
+        public void Redirect_To_Team_Info()
+        {
+            // Arrange
+            Team team = new Team { Name = "Banana", Country = "Cloud", Field = "CPU", Id = 2 };
+            HomeController homeController = new HomeController(_context);
+
+            // Act
+            var result = homeController.Index(team);
+
+
+            // Assert
+            var redirectToActionResult = Assert.IsType<RedirectToActionResult>(result);
+            Assert.Equal("Team", redirectToActionResult.ControllerName);
+            Assert.Equal("TeamInfo", redirectToActionResult.ActionName);
+        }
+    }
+}
