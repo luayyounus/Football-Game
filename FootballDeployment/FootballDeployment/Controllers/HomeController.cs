27a627c1a181a35f3fcc4d52ed46f795a98ce9cf
Luay <luayghazwan@yahoo.com>
2018-01-16 02:21:53 -0800

    Updated files

--- FootballDeployment/FootballDeployment/Controllers/HomeController.cs
+++ FootballDeployment/FootballDeployment/Controllers/HomeController.cs
@@ -5,7 +5,6 @@ using System.Threading.Tasks;
 using FootballDeployment.Data;
 using FootballDeployment.Models;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
